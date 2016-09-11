ce9d97f4637e5a4507ba285f8076a56f6d7df40b
joaocalixto <calixtounicap@gmail.com>
2016-09-11 16:55:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/joaocalixto/resinlayout/MainActivity.java
@@ -0,0 +1,19 @@
+package br.com.joaocalixto.resinlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
